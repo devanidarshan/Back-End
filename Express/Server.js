c3ad6050b0817b572fd3e3d06edacf2c0c6d5106
devanidarshan <130140985+devanidarshan@users.noreply.github.com>
2024-02-23 09:44:40 +0530

    Express

--- Express/Server.js
+++ Express/Server.js
@@ -1,15 +1,26 @@
 const express = require ('express');
-const server = express();           // Create Server
+const server = express();       // Create Server
+const path = require('path');
+
+server.post('/', (req , res) =>{
+     res.send('Post Method');
+});
 
 server.get('/', (req , res) =>{
-     res.end('Welcome To Express .js');
+    res.end('Welcome to Express.js');
 });
-server.get('/user', (req , res) =>{
-    res.end('Welcome To Express .js');
+
+server.put('/', (req , res) =>{
+    res.status(400).json({message:'Put Method Call'});
 });
-server.get('/product', (req , res) =>{
-    res.end('Welcome To Express .js');
-});
+
+server.patch('/', (req , res) =>{
+    res.sendFile(path.join(__dirname,'abc.txt'));
+})
+
+server.delete('/', (req , res) =>{
+    res.sendStatus(201);
+})
 
 server.listen(7777,() =>{
     console.log(`Server start at http://localhost:7777`);
